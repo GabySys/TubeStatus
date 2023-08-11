249e8e3512869a0be5f9717ce11b676aaa507e5b
Gabriela Ignacio <GabrielaIgnacio@tfl.gov.uk>
2023-08-11 08:31:09 +0100

    added unit test for status color

--- src/components/StatusList.test.js
+++ src/components/StatusList.test.js
@@ -5,6 +5,28 @@ import React from 'react'
 test("StatusList renders successfully", () => {
     render(<StatusList lines={mockData} />)
     expect(screen.getByText(/District/i)).toBeInTheDocument();
+    expect(screen.getByText(/Good/i)).toBeInTheDocument();
+})
+
+test("StatusList when severity is 10 then background is green", () => {
+    render(<StatusList lines={mockData} />)
+    const element = screen.getByText(/Good/i).parentElement;
+    const styles = window.getComputedStyle(element);
+    expect(styles.backgroundColor).toBe('green');
+})
+
+test("StatusList when severity is 5 then background is yellow", () => {
+    render(<StatusList lines={mockData} />)
+    const element = screen.getByText(/Part Closure/i).parentElement;
+    const styles = window.getComputedStyle(element);
+    expect(styles.backgroundColor).toBe('orange');
+})
+
+test("StatusList when severity is 1 then background is red", () => {
+    render(<StatusList lines={mockData} />)
+    const element = screen.getByText(/Closed/i).parentElement;
+    const styles = window.getComputedStyle(element);
+    expect(styles.backgroundColor).toBe('red');
 })
 
 const mockData = [
@@ -21,6 +43,13 @@ const mockData = [
         "lineStatuses": [{
             "statusSeverity": 5,
             "statusSeverityDescription": "Part Closure"
+        }],
+    }, {
+        "id": "test",
+        "name": "test",
+        "lineStatuses": [{
+            "statusSeverity": 1,
+            "statusSeverityDescription": "Closed"
         }],
     },
 ]
